2cd53fc0251b6f24a74ced609d46b2519a3b25b5
rojasj84 <rojasj84@gmail.com>
2026-08-20 13:18:14 -0400

    feat: add --modern flag to default GUI execution arguments

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
     # Default to GUI if no arguments are passed
     if len(sys.argv) == 1:
         sys.argv.append("gui")
+        sys.argv.append("--modern")
     main()
